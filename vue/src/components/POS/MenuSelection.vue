3653c8eb06cbefd9a8a24c9000d97b3ae9cb641a
marcvaleros <marcnathanielvaleros@gmail.com>
2022-05-25 00:16:32 +0800

    Revert "Test"
    This reverts commit 326426be8d8bcccd47d589960ff10ecd3df306dc.

--- vue/src/components/POS/MenuSelection.vue
+++ vue/src/components/POS/MenuSelection.vue
@@ -62,8 +62,6 @@
  * FOR @CLICK, CALL A METHOD
  * IN THAT METHOD, CALL THE ACTION THAT WILL CALL THE MUTATION
  */
-
-
 import SecondaryButton from "../Buttons/SecondaryButton.vue";
 
 export default {
